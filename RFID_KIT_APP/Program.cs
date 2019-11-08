0b40c6e89eec14eb154a05e38e834b916117dd4d
abdel <abdel@LAPTOP-DVPH15V5>
2019-11-08 22:45:24 +0100

    git connection esstablish

---
+++ RFID_KIT_APP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RFID_KIT_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

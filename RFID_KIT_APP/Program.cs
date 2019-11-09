14f1c9cfcf47f1f0579774b437ada0817ab00b45
HeiballaAbdellahi <abdellahmedlemin@gmail.com>
2019-11-09 22:32:48 +0100

    itss me

--- RFID_KIT_APP/Program.cs
+++ RFID_KIT_APP/Program.cs
@@ -12,12 +12,12 @@ namespace RFID_KIT_APP
             Console.WriteLine("Please Enter Your Name:");
 
             // Create a string variable and get user input from  the keyboard  and store it in the variable
-            string userNamee = Console.ReadLine();
+            string userName = Console.ReadLine();
 
 
 
             // Print the value of the variable (userName), which will display the input value
-            Console.WriteLine("Your Name is :  " + userNamee);
+            Console.WriteLine("Your Name is :  " + userName);
 
 
            
